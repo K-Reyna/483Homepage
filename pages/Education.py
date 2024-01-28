2acbc7c9a6b6ff68f38193417b60b18e54be6336
K-Reyna <91650651+K-Reyna@users.noreply.github.com>
2024-01-27 16:25:02 -0800

    Add files via upload

---
+++ pages/Education.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown("# Educational Background")
+st.sidebar.markdown("# Educational Background")
+
+'I attended Renton High School in Renton for four years. It had a program called "running start," in which juniors and senioirs could take courses at a community college for free. This is what I did for my last two years of high school at a community college near my house called Renton Technical College.'
+
+'I decided to come to UWB because I prefer smaller classes, compared to Seattle\'s large classes.'
+'What interested me about computer science was the algorithms, I find it really interesting how everything comes together like a puzzle.'
