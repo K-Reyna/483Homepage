5a1a09dde664ab440bb7cf36bac11ccf6f23cc67
K-Reyna <91650651+K-Reyna@users.noreply.github.com>
2024-01-27 16:05:19 -0800

    Add files via upload

---
+++ Personal.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.markdown("# Personal")
+st.sidebar.markdown("# Personal")
+
+'My name is Karen Reynaga. I grew up in Renton, Washington, and still live here to this day.'
+
+'I have a dog named Rocky. He is a mix of border collie, german shepherd, and pointer.'
+st.image('rocky.png', caption='Rocky at 2 years old')
+
+'Some of my hobbies include reading, cooking, and hiking.'
---
+++ main_page.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+import time
+
+
+st.markdown("# Personal Website")
+st.sidebar.markdown("# Main page")
+
+'Hello! Welcome to my personal website'
+'My name is Karen Reynaga. On the left are some pages you can visit to learn more about me.'
